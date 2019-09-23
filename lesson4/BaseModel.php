02658de710379008ac602c4a40718fa083206854
thienth32 <thienth32@gmail.com>
2019-09-23 16:04:26 +0700

    Update BaseModel.php

--- lesson4/BaseModel.php
+++ lesson4/BaseModel.php
@@ -7,10 +7,10 @@ class BaseModel
 		$this->conn = new PDO('mysql:host=127.0.0.1;dbname=php2_lesson1;charset=utf8', 'root', '123456');
 	}
 
-	public function getAll(){
-		$sql = "select * from " . $this->table;
-		// var_dump($sql);die;
-		$stmt = $this->conn->prepare($sql);
+	static function all(){
+		$model = new static();
+		$sql = "select * from " . $model->table;
+		$stmt = $model->conn->prepare($sql);
 		$stmt->execute();
 		$result = $stmt->fetchAll();
 		return $result;
@@ -43,9 +43,8 @@ class Hotel extends BaseModel
 	var $table = "hotels";	
 }
 
-$hotelDao = new Hotel();
-$hotelDao->delete(1);
-$hotels = $hotelDao->getAll();
+
+$hotels = User::all();
 
 var_dump($hotels);
 
