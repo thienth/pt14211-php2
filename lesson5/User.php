497d4e0cdfb982caf86c75cfa716f36224e59b86
thienth32 <thienth32@gmail.com>
2019-09-25 15:10:39 +0700

    update

--- lesson5/User.php
+++ lesson5/User.php
@@ -1,8 +1,5 @@
 <?php 
 require_once './BaseModel.php';
-/**
- * 
- */
 class User extends BaseModel
 {
 	protected $tableName = 'users';
--- lesson5/index.php
+++ lesson5/index.php
@@ -1,6 +1,25 @@
 <?php 
 require_once './User.php';
 $users = User::all();
-var_dump($users);die;
-
  ?>
+<table>
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Name</th>
+			<th>Email</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($users as $key => $value): ?>
+ 		<tr>
+			<td><?php echo $value->id ?></td>
+			<td><?php echo $value->name ?></td>
+			<td><?php echo $value->email ?></td>
+		</tr>
+ 		<?php endforeach ?>
+		
+	</tbody>
+</table>
+
+ 
