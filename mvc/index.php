e614803d6e738133fbd9eef05c9c35d97ab787ae
thienth32 <thienth32@gmail.com>
2019-10-07 15:33:05 +0700

    Update index.php

--- mvc/index.php
+++ mvc/index.php
@@ -13,7 +13,67 @@ switch ($url) {
 		$ctr = new HomeController();
 		$ctr->index();
 		break;
-
+	case 'login':
+		// màn hình form login bao gồm:
+		// email
+		// password
+		break;
+	case 'post_login':
+		// Thực hiện login với db
+		// nếu thành công => admin
+		// nếu không thành công => login
+		break;
+	case 'admin':
+		// Thực hiện hiển thị: 
+		// tổng số lượng danh mục
+		// tổng số lượng sản phẩm
+		// tổng số lượng hóa đơn
+		// tổng số lượng user
+		break;
+	case 'admin/products':
+		// hiển thị danh sách sản phẩm
+		break;
+	case 'admin/products/add':
+		// màn hình thêm sản phẩm
+		break;
+	case 'admin/products/save-add':
+		// lưu sản phẩm từ màn hình thêm sản phẩm
+		// lưu thành công => admin/products
+		// không thành công => admin/products/add
+		break;
+	case 'admin/products/edit':
+		// màn hình sửa sản phẩm
+		break;
+	case 'admin/products/save-edit':
+		// lưu sản phẩm từ màn hình thêm sản phẩm
+		// lưu thành công => admin/products
+		// không thành công => admin/products/edit
+		break;
+	case 'admin/products/remove':
+		// màn hình sửa sản phẩm
+		break;
+	case 'admin/categories':
+		// hiển thị danh sách danh mục
+		break;
+	case 'admin/categories/add':
+		// màn hình thêm danh mục
+		break;
+	case 'admin/categories/save-add':
+		// lưu danh mục từ màn hình thêm danh mục
+		// lưu thành công => admin/categories
+		// không thành công => admin/categories/add
+		break;
+	case 'admin/categories/edit':
+		// màn hình sửa danh mục
+		break;
+	case 'admin/categories/save-edit':
+		// lưu danh mục từ màn hình thêm danh mục
+		// lưu thành công => admin/categories
+		// không thành công => admin/categories/edit
+		break;
+	case 'admin/categories/remove':
+		// màn hình sửa danh mục
+		break;
 	case 'lien-he':
 		$ctr = new HomeController();
 		$ctr->contact();
